4f5a080354b8f72a0c5586d93bd32c0e52d2dedb
WANG Zeping <zepingwong@gmail.com>
2022-12-12 11:12:01 +0800

    workfolow

--- configs/vite/constants/index.ts
+++ configs/vite/constants/index.ts
@@ -4,4 +4,4 @@ export const GLOB_CONFIG_FILE_NAME = "_app.config.js"
 // vite packages output directory
 export const OUTPUT_DIR = "dist"
 
-export const APP_NAME = "scroll-screen"
+export const APP_NAME = "vue3-ant-admin"
